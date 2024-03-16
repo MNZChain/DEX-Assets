a62d2cfe92f618cab7f27c6bc5d70b0be4e6c2dc
aajMnz <162629484+aajMnz@users.noreply.github.com>
2024-03-16 12:35:49 +0530

    Create dex-tokenlistV2.sol

---
+++ dex-tokenlistV2.sol
@@ -0,0 +1,38 @@
+{
+  "name": "Mainnetz LIST",
+  "timestamp": "2023-01-21T23:57:10.982Z",
+  "version": {
+    "major": 2,
+    "minor": 0,
+    "patch": 0
+  },
+  "tags": {},
+  "logoURI": "https://raw.githubusercontent.com/MNZChain/DEX-Assets/main/assets/0x41c515fA7D541bCbDEfB44f0FE2B1629aec140b9.png",
+  "keywords": [
+    "mainnetz",
+    "default"
+  ],
+  "tokens": [
+
+    {
+      "chainId": 2016,
+      "address": "0x41c515fA7D541bCbDEfB44f0FE2B1629aec140b9",
+      "name": "WNETZ",
+      "symbol": "WNETZ",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/MNZChain/DEX-Assets/main/assets/0x41c515fA7D541bCbDEfB44f0FE2B1629aec140b9.png"
+    },
+
+    {
+      "name": "USDT",
+      "address": "0x4B60E77b2541f40448ADC99B61B1f8A1DeeE85c3",
+      "symbol": "USDT",
+      "decimals": 18,
+      "chainId": 2016,
+      "logoURI": "https://raw.githubusercontent.com/MNZChain/DEX-Assets/main/assets/0x4B60E77b2541f40448ADC99B61B1f8A1DeeE85c3.png"
+    }
+
+    
+
+  ]
+}
